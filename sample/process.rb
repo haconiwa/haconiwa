b11c04ee794c7673d671440183d5440698e9672d
Uchio KONDO <udzura@udzura.jp>
2016-06-13 17:37:57 +0900

    Update containing sample

--- sample/process.rb
+++ sample/process.rb
@@ -1,8 +1,18 @@
 Namespace.unshare(Namespace::CLONE_NEWNS)
 
-system "mount --make-private /"
-system "mount --bind /var/lib/myroot /var/lib/haconiwa/root"
+m = Mount.new
+
+m.make_private "/"
+m.bind_mount "/var/lib/myroot", "/var/lib/haconiwa/root"
 
 Namespace.unshare(Namespace::CLONE_NEWPID)
-c = Process.fork { Exec.exec "/bin/sh", "-c", "chroot /var/lib/haconiwa /bin/bash -c 'mount -t proc proc /proc; exec /bin/sh'" }
-Process.waitpid c
+
+Dir.chroot "/var/lib/haconiwa"
+Dir.chdir "/"
+
+c = Process.fork {
+  m.mount "proc", "/proc", :type => "proc"
+  Exec.exec "/bin/sh"
+}
+pid, ret = Process.waitpid2 c
+puts "Container exited with: #{ret.inspect}"
