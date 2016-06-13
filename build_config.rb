a3d7849d892a31d0673b1e80adc400d86372f5c6
Uchio KONDO <udzura@udzura.jp>
2016-06-13 17:31:59 +0900

    Include using mgems

--- build_config.rb
+++ build_config.rb
@@ -6,8 +6,10 @@ MRuby::Build.new do |conf|
   GHQ_ROOT = "/Users/udzura/.ghq/"
   conf.gem :github => 'matsumoto-r/mruby-cgroup'
   conf.gem :github => 'iij/mruby-process'
+  conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'haconiwa/mruby-exec'
   conf.gem :github => 'haconiwa/mruby-namespace'
+  conf.gem :github => 'haconiwa/mruby-mount'
 
   conf.cc do |cc|
     cc.defines = %w(DEBUG) if ENV['DEBUG']
