e483c99e3315efe945ef04ceb94afbee70729e94
Uchio KONDO <udzura@udzura.jp>
2016-06-29 19:36:21 +0900

    Update project gems

--- build_config.rb
+++ build_config.rb
@@ -1,15 +1,17 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'full-core'
-  # conf.gem '.'
 
-  GHQ_ROOT = "/Users/udzura/.ghq/"
   conf.gem :github => 'matsumoto-r/mruby-cgroup'
+  conf.gem :github => 'matsumoto-r/mruby-capability'
+  conf.gem :github => 'harasou/mruby-resource'
   conf.gem :github => 'iij/mruby-process'
   conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'haconiwa/mruby-exec'
   conf.gem :github => 'haconiwa/mruby-namespace'
   conf.gem :github => 'haconiwa/mruby-mount'
+
+  conf.gem '.'
 
   conf.cc do |cc|
     cc.defines = %w(DEBUG) if ENV['DEBUG']
