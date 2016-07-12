6d3c462562c9f8f460c471e428cd6019dd693985
Uchio KONDO <udzura@pepabo.com>
2016-07-12 14:45:49 +0900

    Add very small pathname class

---
+++ mrblib/vendor/minipathname.rb
@@ -0,0 +1,29 @@
+class Pathname
+  def initialize(path)
+    @path = path
+  end
+
+  def join(*paths)
+    @path = clean_slashes([@path, *paths].flatten.join('/'))
+  end
+
+  def to_s
+    @path
+  end
+
+  def to_str
+    to_s
+  end
+
+  def inspect
+    "#<Pathname path=#{@path}>"
+  end
+
+  private
+  def clean_slashes(newpath)
+    while newpath.include?('//')
+      newpath.gsub!('//', '/')
+    end
+    newpath
+  end
+end
