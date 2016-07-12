ff9dcaf393d8a6ee0eec2a8bfe0e87715c6a34c8
Uchio KONDO <udzura@pepabo.com>
2016-07-12 16:55:58 +0900

    Add defs

--- src/haconiwa.c
+++ src/haconiwa.c
@@ -1,10 +1,27 @@
 #include "mruby.h"
+#include "mruby/string.h"
+#include "mruby/hash.h"
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
+struct mrbgem_revision {
+  char* gemname;
+  char* revision;
+};
+
 static mrb_value mrb_haconiwa_mrgbem_versions(mrb_state *mrb, mrb_value self)
 {
-  return mrb_fixnum_value(0);
+  struct mrbgem_revision gems[] = {
+    #include "REVISIONS.defs"
+    {NULL, NULL},
+  };
+  mrb_value ha = mrb_hash_new(mrb);
+
+  for (int i = 0; gems[i].gemname != NULL; ++i) {
+    mrb_hash_set(mrb, ha, mrb_str_new_cstr(mrb, gems[i].gemname), mrb_str_new_cstr(mrb, gems[i].revision));
+  }
+
+  return ha;
 }
 
 void mrb_haconiwa_gem_init(mrb_state *mrb)
