289ce93e0cbbfb4826976f0509b9d106ca333ffa
Uchio KONDO <udzura@pepabo.com>
2016-07-13 17:21:46 +0900

    Fix methods and its name

--- src/haconiwa.c
+++ src/haconiwa.c
@@ -5,20 +5,21 @@
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
 struct mrbgem_revision {
-  char* gemname;
-  char* revision;
+  const char* gemname;
+  const char* revision;
 };
 
-static mrb_value mrb_haconiwa_mrgbem_versions(mrb_state *mrb, mrb_value self)
+const struct mrbgem_revision GEMS[] = {
+  #include "REVISIONS.defs"
+  {NULL, NULL},
+};
+
+static mrb_value mrb_haconiwa_mrgbem_revisions(mrb_state *mrb, mrb_value self)
 {
-  struct mrbgem_revision gems[] = {
-    #include "REVISIONS.defs"
-    {NULL, NULL},
-  };
   mrb_value ha = mrb_hash_new(mrb);
 
-  for (int i = 0; gems[i].gemname != NULL; ++i) {
-    mrb_hash_set(mrb, ha, mrb_str_new_cstr(mrb, gems[i].gemname), mrb_str_new_cstr(mrb, gems[i].revision));
+  for (int i = 0; GEMS[i].gemname != NULL; ++i) {
+    mrb_hash_set(mrb, ha, mrb_str_new_cstr(mrb, GEMS[i].gemname), mrb_str_new_cstr(mrb, GEMS[i].revision));
   }
 
   return ha;
@@ -28,7 +29,7 @@ void mrb_haconiwa_gem_init(mrb_state *mrb)
 {
   struct RClass *haconiwa;
   haconiwa = mrb_define_module(mrb, "Haconiwa");
-  mrb_define_class_method(mrb, haconiwa, "mrbgem_versions", mrb_haconiwa_mrgbem_versions, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, haconiwa, "mrbgem_revisions", mrb_haconiwa_mrgbem_revisions, MRB_ARGS_NONE());
 
   DONE;
 }
