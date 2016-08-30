ceb691ad708f96c08ef3db445fbbd6aee988498f
Uchio KONDO <udzura@pepabo.com>
2016-08-30 11:28:58 +0900

    Add simple secure_random helper

--- mrblib/vendor/uuid.rb
+++ mrblib/vendor/uuid.rb
@@ -3,6 +3,19 @@ module UUID
     Kernel.srand(Time.now.to_i ^ Process.pid)
   end
 
+  def self.secure_random(range=nil)
+    case range
+    when NilClass
+      b = File.read("/dev/urandom", 1).bytes.first + 1
+      1 - (b / 256.0)
+    when Range
+      r = range.to_a
+      r[secure_random(r.size)]
+    else
+      (range * secure_random).floor
+    end
+  end
+
   def self.uuid(fmt="%04x%04x-%04x-%04x-%04x-%04x%04x%04x")
     s = []
     8.times { s << rand(256 * 256) }
