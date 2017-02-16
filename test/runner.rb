4a69c0945990e323a45b01fd80cb7b3e0742290a
pyama86 <pyama@pepabo.com>
2017-02-16 15:00:54 +0900

    fix test

--- test/runner.rb
+++ test/runner.rb
@@ -12,7 +12,7 @@ assert("Haconiwa::LinuxRunner#confirm_existence_pid_file") do
 
   # Because it is a pid that does not exist
   File.open(t.path, "w+") {|f| f.print "-1000" }
-  assert_nothing_raised(runner.send('confirm_existence_pid_file', t.path))
+  assert_nothing_raised("You can delete the pid for processes that do not exist") { runner.send('confirm_existence_pid_file', t.path) }
 
   assert_false(File.exist?(t.path))
 end
