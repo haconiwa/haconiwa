4870b2f1b094fca11fb7d8d1d271206e5639b7af
Uchio Kondo <udzura@udzura.jp>
2017-03-29 19:12:30 +0900

    Rough implement of threaded waitloop

--- mrblib/haconiwa/wait_loop.rb
+++ mrblib/haconiwa/wait_loop.rb
@@ -1,63 +1,60 @@
 module Haconiwa
   class WaitLoop
     def initialize
+      @sig_threads = []
+      @hook_threads = []
       @hooks = []
     end
     attr_reader :hooks
 
     def register_hooks(base)
-      timers = []
       @hooks.each do |hook|
-        t = UV::Timer.new
-        hook.register(t, base)
-        timers << t
+        hook.set_signal!
+        proc = hook.proc
+        @hook_threads << SignalThread.trap(hook.signal) do
+          case proc.arity
+          when 1
+            proc.call(base)
+          when 2
+            proc.call(base, hook.active_timer)
+          else
+          end
+        end
       end
-      timers
     end
 
     def register_sighandlers(base, runner, etcd)
-      done = []
       [:SIGTERM, :SIGINT, :SIGHUP, :SIGPIPE].each do |sig|
-        s = UV::Signal.new()
-        s.start(UV::Signal.const_get(sig)) do |signo|
+        @sig_threads << SignalThread.trap(sig) do
           unless base.cleaned
             Logger.warning "Supervisor received unintended kill. Cleanup..."
             runner.cleanup_supervisor(base, etcd)
           end
-          UV::default_loop.stop()
+          Process.kill :SIGTERM, base.pid
           exit 127
         end
       end
-      done
     end
 
     def register_custom_sighandlers(base, handlers)
-      done = []
       handlers.each do |sig, callback|
-        s = UV::Signal.new()
-        s.start(UV::Signal.const_get(sig)) do |signo|
+        @sig_threads << SignalThread.trap do |signo|
           callback.call(base)
         end
-        done << s
       end
-      done
     end
 
     def run_and_wait(pid)
-      main = UV::Timer.new
-      p = s = nil
-      main.start(50, 50) do
-        p, s = Process.waitpid2(pid, Process::WNOHANG)
-        if p
-          Logger.puts "Container(#{p}) finish detected: #{s.inspect}"
-          UV::default_loop.stop()
-        end
-      end
-      UV::run()
+      p, s = Process.waitpid2(pid)
+      Logger.puts "Container(#{p}) finish detected: #{s.inspect}"
       return [p, s]
     end
 
     class TimerHook
+      def self.signal_pool
+        @__signal_pool = []
+      end
+
       def initialize(timing={}, &b)
         @timing = if s = timing[:msec]
                     s
@@ -70,20 +67,36 @@ module Haconiwa
                   else
                     raise(ArgumentError, "Invalid option: #{timing.inspect}")
                   end
+        @interval = timing[:interval_msec] # TODO: other time scales
         @proc = b
         @id = UUID.secure_uuid
+        @signal = nil
+        @active_timer = nil
       end
-      attr_reader :timing, :proc, :id
+      attr_reader :timing, :proc, :id, :signal, :active_timer
 
-      def register(t, base)
-        t.start(@timing, 0) do
-          begin
-            Logger.debug "The timer hook(#{@id}) invoked after #{@timing} msec"
-            @proc.call(base)
-            Logger.debug "The timer hook(#{@id}) success"
-          rescue => e
-            Logger.debug "Something is wrong on timer hook(#{@id}): #{e.class}/#{e.message}"
+      # This method has a race problem, should be called serially
+      def set_signal!
+        idx = 0
+        while !signal do
+          if TimerHook.signal_pool.include?(:"SIGRT#{idx}")
+            idx += 1
+          else
+            @signal = :"SIGRT#{idx}"
+            TimerHook.signal_pool << @signal
           end
+        end
+      end
+
+      def start
+        if signal
+          t = ::Timer::POSIX.new(signal: signal)
+          if @interval
+            t.run(@timing, @interval)
+          else
+            t.run(@timing)
+          end
+          @active_timer = t
         end
       end
     end
