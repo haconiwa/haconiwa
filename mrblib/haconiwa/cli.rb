1eecf6ecdaa3cd5d5b1d6ba99e55067886eb419c
Uchio Kondo <udzura@udzura.jp>
2017-04-06 17:05:34 +0900

    Fullpathize

--- mrblib/haconiwa/cli.rb
+++ mrblib/haconiwa/cli.rb
@@ -178,7 +178,7 @@ module Haconiwa
     def self.get_base(args)
       script = File.read(args[0])
       obj = Kernel.eval(script)
-      obj.hacofile = args[0]
+      obj.hacofile = (args[0][0] == '/') ? args[0] : File.expand_path(args[0], Dir.pwd)
       return obj
     end
 
@@ -189,7 +189,7 @@ module Haconiwa
         exe.shift
       end
       obj = Kernel.eval(script)
-      obj.hacofile = args[0]
+      obj.hacofile = (args[0][0] == '/') ? args[0] : File.expand_path(args[0], Dir.pwd)
 
       return [obj, exe]
     end
