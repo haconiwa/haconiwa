1e9a85a6b99f7b72112fb3debbad8c605e6fedd5
Uchio Kondo <udzura@udzura.jp>
2018-07-03 15:58:31 +0900

    Add Checkpoint Service

---
+++ mrblib/haconiwa/criu_service.rb
@@ -0,0 +1,15 @@
+module Haconiwa
+  class CRIUService
+    def initialize(base)
+      @base = base
+    end
+
+    def create_checkpoint
+      # TBD
+    end
+
+    def restore
+      # TBD, using LinuxRunner?
+    end
+  end
+end
