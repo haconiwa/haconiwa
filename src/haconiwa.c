93b9233a3eb3a43d1a9c7e9426eac6e9ee7a4a82
Uchio Kondo <udzura@udzura.jp>
2018-10-26 14:58:47 +0900

    Add haconiwa_pivot_root from latest lxc

--- src/haconiwa.c
+++ src/haconiwa.c
@@ -1,10 +1,14 @@
 #define _GNU_SOURCE
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <syslog.h>
+#include <string.h>
+#include <errno.h>
 
 #include "mruby.h"
 #include "mruby/string.h"
@@ -38,36 +42,110 @@ int pivot_root(const char *new_root, const char *put_old){
   return (int)syscall(SYS_pivot_root, new_root, put_old);
 }
 
-/* TODO: This method will be implemented in mruby-procutil */
+#define MRB_SYS_ERROR(mrb, msg)                                         \
+  do {                                                                  \
+    int errno_old = errno;                                              \
+    char buf1[1024];                                                    \
+    char buf2[2048];                                                    \
+    if (strerror_r(errno_old, buf1, 1024) < 0) mrb_sys_fail(mrb, "invalid errno"); \
+    if (snprintf(buf2, 2048, "%s - %s", msg, buf1) < 0) mrb_sys_fail(mrb, "failed to construct message"); \
+    mrb_sys_fail(mrb, buf2);                                            \
+    errno = errno_old;                                                  \
+  } while (0)
+
 /* This function is written after lxc/conf.c
-   https://github.com/lxc/lxc/blob/a467a845443054a9f75d65cf0a73bb4d5ff2ab71/src/lxc/conf.c */
+   https://github.com/lxc/lxc/blob/3695b24384b71662a1225f6cc25f702667fbbe38/src/lxc/conf.c#L1495 */
+static int haconiwa_pivot_root(const char *rootfs)
+{
+  int oldroot;
+  int newroot = -1, ret = -1;
+
+  oldroot = open("/", O_DIRECTORY | O_RDONLY | O_CLOEXEC);
+  if (oldroot < 0) {
+    MRB_SYS_ERROR(mrb, "Failed to open old root directory");
+    return -1;
+  }
+
+  newroot = open(rootfs, O_DIRECTORY | O_RDONLY | O_CLOEXEC);
+  if (newroot < 0) {
+    MRB_SYS_ERROR(mrb, "Failed to open new root directory");
+    goto on_error;
+  }
+
+  /* change into new root fs */
+  ret = fchdir(newroot);
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to change to new rootfs");
+    goto on_error;
+  }
+
+  /* pivot_root into our new root fs */
+  ret = pivot_root(".", ".");
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to pivot_root()");
+    goto on_error;
+  }
+
+  /* At this point the old-root is mounted on top of our new-root. To
+   * unmounted it we must not be chdir'd into it, so escape back to
+   * old-root.
+   */
+  ret = fchdir(oldroot);
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to enter old root directory");
+    goto on_error;
+  }
+
+  /* Make oldroot rslave to make sure our umounts don't propagate to the
+   * host.
+   */
+  ret = mount("", ".", "", MS_SLAVE | MS_REC, NULL);
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to make oldroot rslave");
+    goto on_error;
+  }
+
+  ret = umount2(".", MNT_DETACH);
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to detach old root directory");
+    goto on_error;
+  }
+
+  ret = fchdir(newroot);
+  if (ret < 0) {
+    ret = -1;
+    MRB_SYS_ERROR(mrb, "Failed to re-enter new root directory");
+    goto on_error;
+  }
+
+  ret = 0;
+
+  syslog(LOG_INFO, "pivot_root(\"%s\") successful", rootfs);
+
+on_error:
+  close(oldroot);
+
+  if (newroot >= 0)
+    close(newroot);
+
+  return ret;
+}
+
+/* TODO: This method will be implemented in mruby-procutil */
+
 static mrb_value mrb_haconiwa_pivot_root_to(mrb_state *mrb, mrb_value self)
 {
   char *newroot;
-  int newrootfd = -1;
-
   mrb_get_args(mrb, "z", &newroot);
 
-  newrootfd = open(newroot, O_DIRECTORY | O_RDONLY);
-  if (newrootfd < 0) {
-    mrb_sys_fail(mrb, "open(newroot)");
+  if(haconiwa_pivot_root(mrb, newroot) < 0) {
+    mrb_sys_fail(mrb, "pivot_root failed!!!");
   }
-  if (fchdir(newrootfd) < 0) {
-    mrb_sys_fail(mrb, "fchdir(newroot)");
-  }
-
-  if (pivot_root(".", "./.gc") < 0) {
-    perror("pivot_root");
-    mrb_sys_fail(mrb, "Cannot pivot_root!");
-  }
-
-  if (chdir("/") < 0) {
-    mrb_sys_fail(mrb, "chdir(newroot) back");
-  }
-  if (umount2("/.gc", MNT_DETACH) < 0) {
-    mrb_sys_fail(mrb, "umount2");
-  }
-  close(newrootfd);
 
   return mrb_true_value();
 }
