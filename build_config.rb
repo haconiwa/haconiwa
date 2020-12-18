e070642c5e5b631fd867f2fd4f1f0f2e19973f8b
Uchio Kondo <udzura@udzura.jp>
2020-12-18 20:37:41 +0900

    Only build mruby&mirb

--- build_config.rb
+++ build_config.rb
@@ -18,32 +18,7 @@ MRuby::Build.new do |conf|
   conf.enable_test
 
   gem_config(conf)
-  conf.gem github: 'matsumotory/mruby-localmemcache'
-end
-
-# Just build for Linux...
-MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
-  toolchain :gcc
-
-  conf.enable_debug
-
-  gem_config(conf)
-end
-
-MRuby::Build.new('x86_64-pc-linux-gnu_mirb') do |conf|
-  toolchain :gcc
-
-  gem_config(conf)
+  # conf.gem github: 'matsumotory/mruby-localmemcache'
   conf.gem core: 'mruby-bin-mirb'
-  conf.bins = ["mirb"]
+  conf.bins = ["mruby", "mirb"]
 end
-
-# MRuby::CrossBuild.new('i686-pc-linux-gnu') do |conf|
-#   toolchain :gcc
-
-#   [conf.cc, conf.cxx, conf.linker].each do |cc|
-#     cc.flags << "-m32"
-#   end
-
-#   gem_config(conf)
-# end
